b22d8cfb033687423494ca71a8585ec443b6c5cb
Adnan Raja <58033249+adnannrajaa@users.noreply.github.com>
2025-04-09 13:23:50 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  fileServer: 'https://api.jklibrary.com/Upload',
+  fileServer: 'https://api.jklibrary.com',
   baseUrl: 'https://api.jklibrary.com',
   getIpAddress: 'https://api.ipify.org/?format=json',
   getIpDetails: 'http://ip-api.com/json/',
