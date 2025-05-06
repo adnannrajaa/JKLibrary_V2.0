f597b0dc6d5645bfa0f51a71dd8cb9a48f312cda
Adnan Shan <adnan.shann@hotmail.com>
2025-05-06 16:46:07 +0500

    book detail page update

--- src/app/pages/featured-books/book-detail/book-detail.component.ts
+++ src/app/pages/featured-books/book-detail/book-detail.component.ts
@@ -55,7 +55,7 @@ export class BookDetailComponent implements OnInit, OnDestroy {
         if (response.success) {
           this.bookDetail = response.data;
           this.bookDetail.displayCoverPage = this.getCompletePath(this.bookDetail.displayCoverPage);
-          //this.pdfSrc = this.getCompletePath(this.bookDetail.url);
+          this.pdfSrc = this.getCompletePath(this.bookDetail.url);
           if (this.bookDetail.comments?.length > 0) {
             this.comments = this.buildCommentTree(this.bookDetail.comments);
           }
